76fe0c75a2b1273482d89733ae05760150de800f
xianrenrrr <99616758+xianrenrrr@users.noreply.github.com>
2025-07-30 21:29:45 -0700

    1

--- src/main/java/com/example/demo/ai/shared/KeyframeExtractionServiceImpl.java
+++ src/main/java/com/example/demo/ai/shared/KeyframeExtractionServiceImpl.java
@@ -9,7 +9,6 @@ import com.google.cloud.storage.StorageOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
