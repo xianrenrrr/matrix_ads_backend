d884b9dedeebdbade96b62d41640afbe8b8b8149
xianrenrrr <99616758+xianrenrrr@users.noreply.github.com>
2025-08-10 12:29:49 -0700

    Clean up unused imports in DemoApplication

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
