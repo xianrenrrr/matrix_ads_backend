cb318deebe0b5415ce86db111f3229e9e8d3b656
xianrenrrr <99616758+xianrenrrr@users.noreply.github.com>
2025-11-01 16:12:02 -0700

    12

--- src/main/java/com/example/demo/ai/subtitle/SubtitleSegment.java
+++ src/main/java/com/example/demo/ai/subtitle/SubtitleSegment.java
@@ -64,6 +64,38 @@ public class SubtitleSegment {
         this.confidence = confidence;
     }
     
+    public Integer getTop() {
+        return top;
+    }
+    
+    public void setTop(Integer top) {
+        this.top = top;
+    }
+    
+    public Integer getLeft() {
+        return left;
+    }
+    
+    public void setLeft(Integer left) {
+        this.left = left;
+    }
+    
+    public Integer getWidth() {
+        return width;
+    }
+    
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+    
+    public Integer getHeight() {
+        return height;
+    }
+    
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+    
     /**
      * Get duration in milliseconds
      */
